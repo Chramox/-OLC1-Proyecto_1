a5927a6b062501c7ea74d80e500986c789aa9e10
Chramox <2994775760101@ingenieria.usac.edu.gt>
2020-03-02 13:59:34 -0600

    Agregar archivos de proyecto.

---
+++ Analizador_Lexico.cs
@@ -0,0 +1,449 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _OLC1_Proyecto_1
+{
+    class Analizador_Lexico
+    {
+        //VARIABLES
+        LinkedList<Token> listaTokens = new LinkedList<Token>();
+        LinkedList<Token> listaErrores = new LinkedList<Token>();
+        int idToken;
+        int fila = 1;
+        int columna = 1;
+        int estado = 0; //variable para ir cambiando de estado
+        String auxiliarLexema = ""; //variable donde se guarda el valor del Token
+        bool esCadena = false;
+        bool esComentarioSimple = false;
+        bool esComentarioMultiple = false;
+        bool esNumeroFlotante = false;
+
+        public void separarLineas(String Lineas)
+        {
+            Lineas += "Ç";
+            String[] lines = Lineas.Split('\n');
+            foreach (var line in lines)
+            {
+                Leer(line);
+            }
+            if (listaTokens.Count != 0)
+            {
+                foreach (var Token in listaTokens)
+                {
+                    Console.WriteLine(Token.getValorToken() + "<------>" + Token.getTipoToken());
+                }
+            }
+        }
+        public void Leer(String Texto)
+        {
+            char c;
+            fila++;
+            columna = 1;
+
+            char[] caracteres = Texto.ToCharArray();
+
+            for (int i = 0; i < Texto.Length; i++)
+            {
+                c = caracteres[i];
+
+                if (c == 'Ç' && i == (caracteres.Length - 1))
+                {
+                    if (esCadena || esComentarioSimple || esComentarioMultiple)
+                    {
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("FIN ANALISIS");
+                        foreach (var Token in listaTokens)
+                        {
+                            Console.WriteLine(Token.getValorToken() + "<------>" + Token.getTipoToken());
+                        }
+                        if (listaErrores.Count != 0)
+                        {
+                            foreach (var Token in listaErrores)
+                            {
+                                Console.WriteLine(Token.getValorToken() + "<------>" + Token.getTipoToken());
+                            }
+                        }
+                        break;
+                    }
+                }
+                //IMPLEMENTACION DEL AUTOMATA FINITO
+                switch (estado)
+                {
+                    // ESTADO INICIAL
+                    case 0:
+                        {
+                            if (Char.IsDigit(c))
+                            {
+                                estado = 1;
+                                auxiliarLexema += c;
+                            }
+                            else if (Char.IsLetter(c))
+                            {
+                                estado = 2;
+                                auxiliarLexema += c;
+                            }//EMPIEZA COMPARACION PARA LOS SIMBOLOS
+                            else if (c == '<')
+                            { //COMENTARIO MULTIPLE
+                                auxiliarLexema += c;
+                                estado = 5;
+                            }
+                            else if (c == '\"')
+                            { //COMILLAS DOBLES --> CADENA
+                                estado = 3;
+                            }
+                            /* else if (c == '\'') { //COMILLAS DOBLES --> CADENA
+                                 estado = 3;
+                             }*/
+                            else if (c == '/')
+                            { //COMENTARIO SIMPLE
+                                auxiliarLexema += c;
+                                estado = 5;
+                            }
+                            else if (c == '!')
+                            {
+                                auxiliarLexema += c;
+                                estado = 5;
+                            }
+                            else if (reconocerSimbolos(c.ToString()))
+                            {
+                                //ADENTRO DEL METODO SE ENCARGA DE TODO EL MANEJO
+                            }
+                            else
+                            {
+                                if (c == 'Ç' && i == (caracteres.Length - 1))
+                                {
+                                    foreach (var Token in listaTokens)
+                                    {
+                                        Console.WriteLine(Token.getValorToken() + "<------>" + Token.getTipoToken());
+                                    }
+                                    if (listaErrores.Count != 0)
+                                    {
+                                        foreach (var Token in listaErrores)
+                                        {
+                                            Console.WriteLine(Token.getValorToken() + "<------>" + Token.getTipoToken());
+                                        }
+                                    }
+                                }
+                            }
+
+                        }
+                        break;
+                    case 1://NUMEROS, SI Y SOLO SI, NUMEROS
+                        {
+                            if (Char.IsDigit(c))
+                            {
+                                estado = 1;
+                                auxiliarLexema += c;
+                            }
+                            else if (c == '.')
+                            {
+                                esNumeroFlotante = true;
+                                estado = 1;
+                                auxiliarLexema += c;
+                            }
+                            else if (Char.IsLetter(c))// SE CONVIERTE EN ERROR (ID`S NO PUEDEN EMPEZAR CON NUMERO)
+                            {
+                                estado = 4;
+                                auxiliarLexema += c;
+                            }
+                            else
+                            {
+                                //idToken = 40;
+                                i--;
+                                if (esNumeroFlotante == false)
+                                {
+                                    agregarToken(Token.Tipo.NUMERO_ENTERO);
+                                }
+                                else // esNumeroFlotante == true -> se agreaga un numero flotante
+                                {
+                                    agregarToken(Token.Tipo.NUMERO_FLOTANTE);
+                                }
+                            }
+                        }
+                        break;
+                    case 2://RESERVADAS Y LETRAS
+                        {
+                            if (Char.IsLetter(c))
+                            {
+                                estado = 2;
+                                auxiliarLexema += c;
+                            }
+                            else if (Char.IsDigit(c))// ID`S PUEDEN TERMINAR CON NUMERO
+                            {
+                                estado = 2;
+                                auxiliarLexema += c;
+                            }
+                            else if (c == '_')
+                            {
+                                estado = 2;
+                                auxiliarLexema += c;
+                            }
+                            else
+                            {
+                                i--;
+                                auxiliarLexema = auxiliarLexema.Trim();
+                                palabrasReservadas(auxiliarLexema);  //COMPARAR PARA VER SI COINCIDE CON ALGUAN PALABRA RESERVADA
+                                                                     //DE LO CONTRARIO ES UN IDENTIFICADOR
+                            }
+                            break;
+                        }
+                    case 3: //CADENAS
+                        {
+                            if (c == '"' && esCadena == false)
+                            {
+                                estado = 3;
+                                esCadena = true;
+                            }
+
+                            if (c == '"' && esCadena == true)
+                            {
+                                esCadena = false;
+                                idToken = 41;
+                                agregarToken(Token.Tipo.CADENA);
+                                estado = 0;
+                            }
+                            else //SIGUE CONCATENANDO HASTA QUE ENCUENTRA LA SEGUNDA COMIILA DOBLE
+                            {
+                                auxiliarLexema += c;
+                                estado = 3;
+                                esCadena = true;
+                            }
+                        }
+                        break;
+                    case 4: // ESTADO DE ERRORES
+                        {
+                            if (Char.IsLetterOrDigit(c))//para que reconozca todo el error
+                            {
+                                auxiliarLexema += c;
+                                estado = 4;
+                            }
+                            else
+                            {
+                                i--;
+                                agregarErrores();
+                                estado = 0;
+                            }
+                            break;
+                        }
+                    case 5: //COMENTARIOS
+                        {
+                            // Console.WriteLine(auxiliarLexema);
+                            //  Console.WriteLine(c);
+                            estado = 5;
+                            if ((i + 1) == caracteres.Length && esComentarioSimple)
+                            {//FIN COMENTARIO SIMPLE
+                                esComentarioSimple = false;
+                                agregarToken(Token.Tipo.COMENTARIO_SIMPLE);
+                            }
+                            else if (c == '/' && esCadena == false)
+                            {
+                                auxiliarLexema += c;
+                                esComentarioSimple = true;
+                            }
+                            else if (c == '!' && esCadena == false) // PARA COMENTARIOS <! 
+                            {
+                                auxiliarLexema += c;
+                                esComentarioMultiple = true;
+                                //                        reconocerSimbolos(auxiliarLexema);
+                            }
+                            else if (c == '>' && esCadena == false && auxiliarLexema.EndsWith("!")) // PARA COMENTARIOS !>
+                            {
+                                auxiliarLexema += c;
+                                //reconocerSimbolos(auxiliarLexema);
+                                agregarToken(Token.Tipo.COMENTARIO_MULTILINEA);
+                                esComentarioMultiple = false;
+                            }
+                            else if (esComentarioMultiple || esComentarioSimple && c != 'Ç')
+                            {
+                                auxiliarLexema += c;
+                            }
+                            else // MANDA LOS OPERADORES DE UN SOLO CARACTER, MANDA EL SEGUNDO CARACTER OTRA VEZ A ANALISIS
+                            {
+                                i--;
+                                if (reconocerSimbolos(auxiliarLexema))
+                                {
+
+                                }
+                                else { estado = 0; }
+
+                            }
+                            break;
+                        }
+                }
+            }
+        }
+
+        public void agregarToken(Token.Tipo Tipo)
+        {
+            listaTokens.AddLast(new Token(Tipo, auxiliarLexema, idToken, fila, columna));
+            auxiliarLexema = "";
+            estado = 0;
+        }
+        public void palabrasReservadas(String auxiliarLexema)
+        {
+            Console.WriteLine("Reservada -- Lexema: " + auxiliarLexema);
+            //AGREGAR TODAS LAS PALABRAS RESERVADAS
+            if (auxiliarLexema.Equals("CONJ"))
+            {
+                agregarToken(Token.Tipo.CONJUNTO);
+            }
+            else
+            {
+                agregarToken(Token.Tipo.IDENTIFICADOR);
+            }
+
+        }
+        public void agregarErrores()
+        {
+            //idToken = 0
+            listaErrores.AddLast(new Token(Token.Tipo.ERROR, auxiliarLexema, idToken, fila, columna));
+            estado = 0;
+        }
+        public void reconocerComentarios(String auxiliarLexema)
+        {
+            auxiliarLexema = auxiliarLexema.Trim();
+            Console.WriteLine("Simbolos que pueden ser dobles " + auxiliarLexema);
+            if (auxiliarLexema.Equals("<!"))
+            { //INICIO COMENTARIO
+
+            }
+            else if (auxiliarLexema.Equals("!>"))
+            { //FIN DE COMENTARIO MULTILINEA
+
+            }
+            else if (auxiliarLexema.Equals("//"))
+            { //COMENTARIO DE UNA SOLA LINEA
+
+            }
+        }
+        private bool reconocerSimbolos(String c)
+        {
+            if (null == c) { return false; }
+            else // ADMIRACION CIERRE, MENOR QUE Y DIAGONAL CASOS ESPECIALES TAMBIEN CASO ESPECIAL LAS COMILLAS --> CADENAS
+                switch (c)
+                {
+                    case "#":
+                        idToken = 35;
+                        agregarToken(Token.Tipo.NUMERAL);
+                        return true;
+                    case "$":
+                        idToken = 36;
+                        agregarToken(Token.Tipo.DOLAR);
+                        return true;
+                    case "%":
+                        idToken = 37;
+                        agregarToken(Token.Tipo.PORCENTAJE);
+                        return true;
+                    case "&":
+                        idToken = 38;
+                        agregarToken(Token.Tipo.I_BONITA);
+                        return true;
+                    case "'":
+                        idToken = 39;
+                        agregarToken(Token.Tipo.COMILLA_SIMPLE);
+                        return true;
+                    case "(":
+                        idToken = 40;
+                        agregarToken(Token.Tipo.PARENTESIS_APERTURA);
+                        return true;
+                    case ")":
+                        idToken = 41;
+                        agregarToken(Token.Tipo.PARENTESIS_CIERRE);
+                        return true;
+                    case "*":
+                        idToken = 42;
+                        agregarToken(Token.Tipo.ASTERISCO);
+                        return true;
+                    case "+":
+                        idToken = 43;
+                        agregarToken(Token.Tipo.OP_SUMA);
+                        return true;
+                    case ",":
+                        idToken = 44;
+                        agregarToken(Token.Tipo.COMA);
+                        return true;
+                    case "-":
+                        idToken = 45;
+                        agregarToken(Token.Tipo.GUION);
+                        return true;
+                    case ".":
+                        idToken = 46;
+                        agregarToken(Token.Tipo.PUNTO);
+                        return true;
+                    case "/":
+                        idToken = 47;
+                        agregarToken(Token.Tipo.DIAGONAL);
+                        return true;
+                    case ":":
+                        idToken = 58;
+                        agregarToken(Token.Tipo.DOS_PUNTOS);
+                        return true;
+                    //OMITIENDO MENOR QUE (60) POR QUE SE USA PARA COMENTARIOS (VIENE PRIMERO LUEGO EL SIGNO DE ADMIRACION)
+                    case ";":
+                        idToken = 59;
+                        agregarToken(Token.Tipo.PUNTO_COMA);
+                        return true;
+                    case "=":
+                        idToken = 61;
+                        agregarToken(Token.Tipo.IGUAL);
+                        return true;
+                    case ">":
+                        idToken = 62;
+                        agregarToken(Token.Tipo.MAYOR);
+                        return true;
+                    case "?":
+                        idToken = 63;
+                        agregarToken(Token.Tipo.PREGUNTA_CIERRE);
+                        return true;
+                    case "@":
+                        idToken = 64;
+                        agregarToken(Token.Tipo.ARROBA);
+                        return true;
+                    case "[":
+                        idToken = 91;
+                        agregarToken(Token.Tipo.CORCHETE_APERTURA);
+                        return true;
+                    case "\\":
+                        idToken = 92;
+                        agregarToken(Token.Tipo.DIAGONAL_INVERSO);
+                        return true;
+                    case "]":
+                        idToken = 93;
+                        agregarToken(Token.Tipo.CORCHETE_CIERRE);
+                        return true;
+                    case "^":
+                        idToken = 94;
+                        agregarToken(Token.Tipo.ELEVAR);
+                        return true;
+                    case "_":
+                        idToken = 95;
+                        agregarToken(Token.Tipo.GUION_BAJO);
+                        return true;
+                    case "`":
+                        idToken = 96;
+                        agregarToken(Token.Tipo.APOSTROFE);
+                        return true;
+                    case "{":
+                        idToken = 123;
+                        agregarToken(Token.Tipo.LLAVE_APERTURA);
+                        return true;
+                    case "|":
+                        idToken = 124;
+                        agregarToken(Token.Tipo.PALITO_OR);
+                        return true;
+                    case "}":
+                        idToken = 125;
+                        agregarToken(Token.Tipo.LLAVE_CIERRE);
+                        return true;
+                    default:
+                        return false;
+                }
+        }
+    }
+}
---
+++ Token.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _OLC1_Proyecto_1
+{
+    class Token
+    {
+        //ATRIBUTOS DE LA CLASE
+        private Tipo tipoToken;
+        private String valorToken;
+        private int ID; //ID DE LOS TOKEN VA A SER SU CODIGO ASCII
+        private int columna;
+        private int fila;
+
+        public enum Tipo
+        {
+            // SIMBOLOS ESPECIALES POR CODIGO ASCII
+            ADMIRACION_CIERRE,  //  !   33
+            COMILLA_DOBLE,      //  "   34
+            NUMERAL,            //  #   35
+            DOLAR,              //  $   36
+            PORCENTAJE,         //  %   37
+            I_BONITA,           //  &   38
+            COMILLA_SIMPLE,     //  '   39
+            PARENTESIS_APERTURA,//  (   40
+            PARENTESIS_CIERRE,  //  )   41
+            ASTERISCO,          //  *   42
+            OP_SUMA,            //  +   43
+            COMA,               //  ,   44
+            GUION,              //  -   45
+            PUNTO,              //  .   46
+            DIAGONAL,           //  /   47
+
+            DOS_PUNTOS,         //  :   58
+            PUNTO_COMA,         //  ;   59
+            MENOR,              //  <   60
+            IGUAL,              //  =   61
+            MAYOR,              //  >   62
+            PREGUNTA_CIERRE,    //  ?   63
+            ARROBA,             //  @   64
+
+            CORCHETE_APERTURA,  //  [   91
+            DIAGONAL_INVERSO,   //  \   92
+            CORCHETE_CIERRE,    //  ]   93
+            ELEVAR,             //  ^   94
+            GUION_BAJO,         //  _   95
+            APOSTROFE,          //  `   96
+
+            LLAVE_APERTURA,     //  {   123
+            PALITO_OR,          //  |   124              
+            LLAVE_CIERRE,       //  }   125
+            //TOKENS NECESARIOS PARA QUE EL PROGRAMA FUNCIONE
+            NUMERO_ENTERO,
+            NUMERO_FLOTANTE,
+            CADENA,
+            ERROR,
+            IDENTIFICADOR,
+            // COMENTARIOS
+            COMENTARIO_SIMPLE, // //
+            COMENTARIO_MULTILINEA, // <!
+                                   // !>
+                                   // -----------------------------
+                                   // PALABRAS RESERVADAS
+            CONJUNTO
+
+
+
+            // [NO SE CUALES VAN A SER MIS PALABRAS RESERVADAS TODAVIA]
+            //        TIPOS DE VARIABLES
+            //        INT, //    
+            //        FLOAT, //     
+            //        CHAR, //     
+            //        STRING, //     
+            //        BOOL, //    
+        }
+        public Token() { } // NADA MAS INICIALIZANDO
+        public Token(Tipo type, String val, int id, int fila, int columna)
+        {
+            this.tipoToken = type;
+            this.ID = id;
+            this.valorToken = val;
+            this.fila = fila;
+            this.columna = columna;
+        }
+
+        public String getValorToken()
+        {
+            return valorToken;
+        }
+
+        public Tipo getTipoToken()
+        {
+            return tipoToken;
+
+        }
+    }
+
+}
